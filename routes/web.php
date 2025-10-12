a1475117dd24a4fb470566005fcb819e99351657
Huyyun_Hyun <96609566+Huyyun@users.noreply.github.com>
2025-10-12 19:07:26 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 
 
-// Route::get('/', function () {
-//     return view('register');
-// });
 
-// Route::post('/register', [HomeController::class, 'register'])->name('register');
-// Route::post('/login', [HomeController::class, 'login'])->name('login');
-// Route::get('/account', [HomeController::class, 'account']);
-// Route::post('/logout', [HomeController::class, 'logout'])->name('logout');
-
+// ===FE ROUTE===========================================================================
 
 Route::get('/', [LoginController::class, 'index'])->name('index');
 Route::get('/register', [UserController::class, 'register'])->name('register');
 Route::get('/login', [UserController::class, 'login'])->name('login');
-Route::post('/store', [UserController::class, 'store'])->name('store');
-Route::post('/storeLogin', [UserController::class, 'storeLogin'])->name('storeLogin');
 Route::get('/logout', [UserController::class, 'logout'])->name('logout');
 Route::get('/account', [UserController::class, 'account'])->name('account');
 Route::get('/logout_up', [UserController::class, 'logout_up'])->name('logout_up');
 
-// Route::post('/login', [LoginController::class, 'validateUser'])->name('login');
-// Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
-Route::get('/content', [LoginController::class, 'checkLogin']);
-// Route::post('/register', [LoginController::class, 'register'])->name('register');
+Route::post('/store', [UserController::class, 'store'])->name('store');
+Route::post('/storeLogin', [UserController::class, 'storeLogin'])->name('storeLogin');
+
+
