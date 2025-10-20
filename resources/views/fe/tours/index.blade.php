a0c3757723c3d87a4039b824b7d5c437508e35c7
iBihari <ngnhubac41113@gmail.com>
2025-10-21 01:20:33 +0700

    tourdetail 2

--- resources/views/fe/tours/index.blade.php
+++ resources/views/fe/tours/index.blade.php
@@ -1,281 +1,140 @@
 @extends('fe.layouts.app')
 
-@section('title', 'Tour Details')
+@section('title', 'Listings')
 
 @section('head')
-    <link rel="stylesheet" href="{{ asset('assets/css/user/tourDetail.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/user/listings.css') }}">
 @endsection
+
 @section('content')
-    <div class="content">
-    <div class="content-header-listings">
-        <div class="left-ct-header">
-            <img src="{{ asset('assets/images/profile-detail.png') }}" alt="Tour Image">
-            <div class="desc-profile">
-                <h4>{{ $tour->name }}</h4>
-                <div class="phone-location">
-                    <img src="{{ asset('assets/images/pin.png') }}" alt="" id="pin-icon">
-                    <span>{{ $tour->address->district }}, {{ $tour->address->city }}</span>
-                    <img src="{{ asset('assets/images/rating-listings-star.png')}}" alt="" id="star-ratings">
-                </div>
-            </div>
+<div class="content">
+        <div class="listings-header">
+            <h2>Listings</h2>
+            <p>Home / Listings</p>
         </div>
-    </div>
-        <div class="content-main-image">
-            <div class="left-main-img">
-                <img src="{{ asset($tour->imageTour) }}" alt="ls-detail">
+        <div class="content-listings">
+            <div class="search-filter-listings">
+                <div class="what-are-you-looking-for-filter">
+                    <input type="search" name="" id="" placeholder="What are you looking for?">
+                </div>
+                <div class="categories-filter">
+                    <p>All Categories</p>
+                    <i class="fa-solid fa-angle-down"></i>
+                </div>
+                <div class="location-filter">
+                    <p>Location</p>
+                    <i class="fa-solid fa-angle-down"></i>
+                </div>
+                <div class="radius-around-filter">
+                    <p>Radius around selected destination</p>
+                    <input type="range" name="" id="" min="0" max="160" value="50">
+                    <p id="kilometer-range">50 km</p>
+                </div>
+                <div class="price-range-filter">
+                    <p>Price range</p>
+                    <i class="fa-solid fa-angle-down"></i>
+                </div>
+                <div class="tags-filter">
+                    <h4>Tags</h4>
+                    <div class="tags-box">
+                        <input type="checkbox" name="" id="credit-card">
+                        <label for="credit-card">Accepts Credit Cards</label>
+                    </div>
+                    <div class="tags-box">
+                        <input type="checkbox" name="" id="smoking-allowed">
+                        <label for="smoking-allowed">Smoking Allowed</label>
+                    </div>
+                    <div class="tags-box">
+                        <input type="checkbox" name="" id="bike-parking">
+                        <label for="bike-parking">Bike Parking</label>
+                    </div>
+                    <div class="tags-box">
+                        <input type="checkbox" name="" id="street-parking">
+                        <label for="street-parking">Street Parking</label>
+                    </div>
+                    <div class="tags-box">
+                        <input type="checkbox" name="" id="wireless-internet">
+                        <label for="wireless-internet">Wireless Internet</label>
+                    </div>
+                    <div class="tags-box">
+                        <input type="checkbox" name="" id="alcohol">
+                        <label for="alcohol">Alcohol</label>
+                    </div>
+                    <div class="tags-box">
+                        <input type="checkbox" name="" id="pet-friendly">
+                        <label for="pet-friendly">Pet Friendly</label>
+                    </div>
+                </div>
+                <button id="search-filter">Search</button>
+                <p id="reset-filter">Reset Filter</p>
             </div>
-            <div class="right-small-image">
-                <div class="img-small-desc">
-                    <img src="{{ asset('assets/images/right-img1.png')}}" alt="Image 1">
-                </div>
-                <div class="img-small-desc">
-                    <img src="{{ asset('assets/images/right-img2.png')}}" alt="Image 2">
-                </div>
-                <div class="img-small-desc">
-                    <img src="{{ asset('assets/images/right-img3.png')}}" alt="Image 3">
-                </div>
-                <div class="img-small-desc">
-                    <img src="{{ asset('assets/images/right-img4.png')}}" alt="Image 4">
-                </div>
-                <div class="img-small-desc">
-                    <img src="{{ asset('assets/images/right-img5.png')}}" alt="Image 5">
-                </div>
-                <div class="img-small-desc">
-                    <img src="{{ asset('assets/images/right-img6.png')}}" alt="Image 6">
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="second-content">
-        <div class="left-overview-content">
-            <h5>Overview</h5>
-            <div class="overview-hotel">
-                <p class="paragraph" id="para-text">{{$tour->description}}</p>
-                <p id="hide-and-show-text">Show more</p>
-            </div>
-            <h5>Amenities</h5>
-            <div class="amenities-tags">
-                <div class="amenities-box">
-                    <div class="box-icon">
-                        <i class="fa-regular fa-credit-card"></i>
+            <div class="listings-container">
+                <div class="listings-container-header">
+                    <div class="showing-results">
+                        <span class="color-span">Showing</span>
+                        <span>1 - 8 of 10 results</span>
                     </div>
-                    <p>Accepts Credit Cards</p>
-                </div>
-                <div class="amenities-box">
-                    <div class="box-icon">
-                        <i class="fa-solid fa-bicycle"></i>
-                    </div>
-                    <p>Bike Parking</p>
-                </div>
-                <div class="amenities-box">
-                    <div class="box-icon">
-                        <i class="fa-solid fa-car"></i>
-                    </div>
-                    <p>Parking Street</p>
-                </div>
-                <div class="amenities-box">
-                    <div class="box-icon">
-                        <i class="fa-solid fa-wifi"></i>
-                    </div>
-                    <p>Wireless Internet</p>
-                </div>
-                <div class="amenities-box">
-                    <div class="box-icon">
-                        <i class="fa-solid fa-wheelchair"></i>
-                    </div>
-                    <p>Wheelchair Accessible</p>
-                </div>
-                <div class="amenities-box">
-                    <div class="box-icon">
-                        <i class="fa-solid fa-paw"></i>
-                    </div>
-                    <p>Pets Friendly</p>
-                </div>
-            </div>
-            <h5>Frequently Asked Questions</h5>
-            <div class="FAQ">
-                <div class="faq-box">
-                    <div class="header-faq">
-                        <h6>What is the Check-in and check-out time?</h6>
-                        <i class="fa-solid fa-plus"></i>
-                    </div>
-                    <div class="content-faq">
-                        <p>Check-in time: 3:00PM <br>
-                        Check-out time: 11:00AM <br>
-                        Please note that these times can vary depending on the specific rental property and the policies set by the homeowner or property management. It's always recommended to confirm the exact check-in and check-out times with the property owner or rental agency when making a reservation.</p>  
-                    </div>
-                </div>
-                <div class="faq-box">
-                    <div class="header-faq">
-                        <h6>How far is the Hotel from airport?</h6>
-                        <i class="fa-solid fa-plus"></i>
-                    </div>
-                    <div class="content-faq">
-                        <p>Certainly, the hotel is situated at a distance of approximately 6 kilometers from the airport.</p>
-                    </div>
-                </div>
-                <div class="faq-box">
-                    <div class="header-faq">
-                        <h6>Do you have cocktails at the bar?</h6>
-                        <i class="fa-solid fa-plus"></i>
-                    </div>
-                    <div class="content-faq">
-                        <p>Certainly, our establishment does indeed offer a selection of cocktails at the bar for your enjoyment.</p>  
-                    </div>
-                </div>
-            </div>
-            <h5>Video</h5>
-            <img src="{{ asset('assets/images/video-listing-detail.png')}}" alt="video-listing-detail" width="100%">
-        </div>
-        <div class="right-price-content">
-            <div class="price-box-book">
-                <span id="price-amount">{{$tour->cost}} $</span>
-                <span id="room-night"> / person</span>
-                <table>
-                    <tr>
-                      <th id="th-border">
-                        <p class="cin-cout">CHECK - IN</p>
-                        <p class="date-ch">{{$tour->startDay}}</p>
-                      </th>
-                      <th>
-                        <p class="cin-cout">CHECK - OUT</p>
-                        <p class="date-ch">{{$tour->endDay}}</p>
-                      </th>
-                    </tr>
-                    <tr>
-                      <td colspan="2" id="td-border">
-                        <p>GUEST</p>
-                        <input type="text">
-                      </td>
-                    </tr>
-                </table>
-                <h6>Price detail</h6>
-                <div class="price-detail-booking">
-                    <div class="fee-row">
-                        <p>$18 x 5 nights</p>
-                        <p class="fee-b">$90</p>
-                    </div>
-                    <div class="fee-row">
-                        <p>Service fee</p>
-                        <p class="fee-b">$30</p>
-                    </div>
-                </div>
-                <div class="total-price">
-                    <h6>Total</h6>
-                    <p>$120</p>
-                </div>
-                <button id="btn-book">Book now</button>
-            </div>
-            <div class="location-booking-hotel">
-                <h5>Location</h5>
-                <img src="./images/map-hotel.png" alt="map-hotel" width="100%", height="auto">
-                <div class="desc-lc-booking">
-                    <div class="lct-box address">
-                        <i class="fa-solid fa-location-dot"></i>
-                        <div>    
-                            <p>01 Độc Lập, Quán Thánh, Ba Đình, Hà Nội</p>
-                            <p id="direct-map">Get Directions</p>
+                    <div class="sort-listings">
+                        <div class="sort-by">
+                            <span class="color-span">Sort By:</span>
+                            <span>Default</span>
+                            <i class="fa-solid fa-angle-down"></i>  
+                        </div>
+                        <div class="arrange-filter-menu">
+                            <img src="{{asset('assets/images/grid-arrange.png')}}" alt="grid">
+                            <img src="{{asset('assets/images/list-menu.png')}}" alt="menu">
                         </div>
                     </div>
-                    <div class="lct-box phone-number">
-                        <i class="fa-solid fa-phone"></i>
-                        <p>080 41532</p>
-                    </div>
-                    <div class="lct-box email">
-                        <i class="fa-regular fa-envelope"></i>
-                        <p>lmao@gmail.com</p>
-                    </div>
-                    <div class="lct-box link">
-                        <i class="fa-solid fa-link"></i>
-                        <p>www.guido.com</p>
-                    </div>
                 </div>
-            </div>
-        </div>   
-    </div>
-    <div class="third-content">
-        <div class="exclusive">    
-            <h3>Exclusive deals</h3>
-            <p>Discover some of the most popular listings in Vietnam based on user reviews and ratings</p>
-        </div>
-        <div class="list-container">
-            <div class="list-box" id="navi-to-ls-detail">
-                <div class="lr-box">
-                    <div class="upper-img">
-                        <img src="./images/hcm1.png" alt="">
+                <div class="listings-list-booking">
+                @foreach($tours as $tour)
+                    <div class="list-box" id="navi-to-ls-detail">
+                        <div class="lr-box">
+                            <div class="upper-img">
+                                <img src="{{ $tour->imageTour }}" alt="">
+                            </div>
+                            <div class="price">
+                                <a href="">{{ number_format($tour->cost, 0, ',', '.') }} $</a>
+                            </div>
+                            <div class="icon-heart">
+                                <i class="fa-regular fa-heart"></i>
+                            </div>
+                            @php
+                                $randomImageIndex = rand(1, 3);
+                            @endphp
+                            <img src="{{ asset('/assets/images/star_9.png') }}" alt="" class="star-ratings">
+                            <img src="{{ asset('/assets/images/profile' . $randomImageIndex . '.png') }}" alt="" class="profile-lr">
+                        </div>
+                        <div class="desc-of-lr">
+                            <div class="location">
+                                <img src="{{ asset('/assets/images/pin.png')}}" alt="">
+                                <span>{{ $tour->address->district }}, {{ $tour->address->city }}</span>
+                            </div>
+                            <h4>{{ $tour->name }}</h4>
+                            <p>{{ $tour->description }}</p>
+                        </div>
+                        <a href="{{ route('tours.show', $tour->idTour) }}">
+                            <button>Book now</button>
+                        </a>
+
                     </div>
-                    <div class="price">
-                        <a href="">$120/ night</a>
-                    </div>
-                    <div class="icon-heart">
-                        <i class="fa-regular fa-heart"></i>
-                    </div>
-                    <img src="./images/star_9.png" alt="" class="star-ratings">
-                    <img src="./images/profile3.png" alt="" class="profile-lr">
+                @endforeach
                 </div>
-                <div class="desc-of-lr">
-                    <div class="location">
-                        <img src="./images/pin.png" alt="">
-                        <span>District 1, Ho Chi Minh</span>
-                    </div>
-                    <h4>Spacious family home with modern amenities</h4>
-                    <p>Located in a vibrant neighbourhood, near an array of trendy shops, restaurants, cafe etc.</p>
+      
+                <div class="pagination">
+                    <img src="{{asset("assets/images/Vector-1.png")}}" alt="" id="vector1">
+                    <p class="pagi">1</p>
+                    <p class="pagi">2</p>
+                    <p class="pagi">3</p>
+                    <p class="pagi">4</p>
+                    <p class="pagi">5</p>
+                    <img src="{{asset("assets/images/Vector-2.png")}}" alt="" id="vector2">
                 </div>
-                <button>Book now</button>
-            </div>
-            <div class="list-box">
-                <div class="lr-box">
-                    <div class="upper-img">
-                        <img src="./images/hcm2.png" alt="">
-                    </div>
-                    <div class="price">
-                        <a href="">$120/ night</a>
-                    </div>
-                    <div class="icon-heart">
-                        <i class="fa-regular fa-heart"></i>
-                    </div>
-                    <img src="./images/star_9.png" alt="" class="star-ratings">
-                    <img src="./images/profile3.png" alt="" class="profile-lr">
-                </div>
-                <div class="desc-of-lr">
-                    <div class="location">
-                        <img src="./images/pin.png" alt="">
-                        <span>District 1, Ho Chi Minh</span>
-                    </div>
-                    <h4>Serene house with breathtaking lakeviews</h4>
-                    <p>Enjoy the view straight from the dining room, and also only 15 mins downtown.</p>
-                </div>
-                <button>Book now</button>
-            </div>
-            <div class="list-box">
-                <div class="lr-box">
-                    <div class="upper-img">
-                        <img src="./images/hcm3.png" alt="">
-                    </div>
-                    <div class="price">
-                        <a href="">$120/ night</a>
-                    </div>
-                    <div class="icon-heart">
-                        <i class="fa-regular fa-heart"></i>
-                    </div>
-                    <img src="./images/star_9.png" alt="" class="star-ratings">
-                    <img src="./images/profile3.png" alt="" class="profile-lr">
-                </div>
-                <div class="desc-of-lr">
-                    <div class="location">
-                        <img src="./images/pin.png" alt="">
-                        <span>District 3, Ho Chi Minh</span>
-                    </div>
-                    <h4>Elegant house with panoramic views</h4>
-                    <p>Offering breathtaking skyline view and close proximity to high-end shopping mall.</p>
-                </div>
-                <button>Book now</button>
             </div>
         </div>
-        <button id="view-all-ls">View all listings</button>
     </div>
 @endsection
 
 @section('scripts')
-    <script src="{{ asset('assets/js/tourDetail.js') }}"></script>
+    <script src="{{ asset('assets/js/listings.js') }}"></script>
 @endsection
